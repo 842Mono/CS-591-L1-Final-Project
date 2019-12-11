1a7b9ddaff196ddd04894f377b8f0f808f160c07
842mono <842mono@gmail.com>
2019-12-11 13:39:59 -0500

    renaming

--- main.js
+++ main.js
@@ -18,11 +18,21 @@ fs.readFile('testcase1.js', 'utf8', function(err, tc1)
         if(err)
             console.log(err);
 
-        let out1 = babel.transform(tc1c, { plugins: [
+        babel.transform(tc1c, { plugins: [
         {
             visitor: {
-                FunctionDeclaration(path) {
-                    console.log(path.node.id.name);
+                FunctionDeclaration(path)
+                {
+                    path.traverse(
+                    {
+                        CallExpression(p2)
+                        {
+                            if(p2.node.callee.name == path.node.id.name)
+                                p2.node.callee.name += "_d"
+                        }
+                    });
+
+                    // console.log(fc);
 
                     decorators[path.node.id.name] = path.node
                     // console.log(decorators);
@@ -30,73 +40,22 @@ fs.readFile('testcase1.js', 'utf8', function(err, tc1)
             }
         }]});
 
-        console.log(">>>>>>>>>>>>>>>>>>>>>>>>>DIVIDER<<<<<<<<<<<<<<<<<<<<<<<<<<");
-
         let out2 = babel.transform(tc1, { plugins: [
         {
             visitor: {
-                FunctionDeclaration(path) {
-
-                    console.log("run!!!!");
-                    console.log(path.node.id.name);
-                    
+                FunctionDeclaration(path)
+                {
                     if(!hasrun.includes(path.node.id.name) && path.node.id.name in decorators)
                     {
                         path.insertAfter(decorators[path.node.id.name]);
                         hasrun.push(path.node.id.name);
+
+                        path.node.id.name += "_d";
                     }
-
-                    // path.parentPath.parentPath.insertAfter(decorators[path.parent.id.name]);
-
-
-                    // console.log(path.parent.id.name);
-                    // console.log(weaves)
-                    
-                    // if(path.parent.id && path.parent.id.name in weaves)
-                    // {
-                    //     console.log(">><<")
-                        // bs = path.get("BlockStatement");
-                        // bs.remove();
-
-                        // sb = path.getSibling()
-                        // console.log(sb);
-
-                        // sb = path.get("right")
-                        // path.insertAfter(babel.types.variableDeclarator(["test"])) //(weaves[path.parent.id.name]));
-
-
-                        // path.insertAfter(babel.parse("let x"));
-
-
-
-
-                        // path.parentPath.parentPath.insertAfter(babel.types.assignmentExpression(
-                        //     babel.types.variableDeclaration("let", [
-                        //         babel.types.variableDeclarator(
-                        //             babel.types.tsParameterProperty(babel.types.identifier(weaves[path.parent.id.name]))
-                        //         )
-                        //     ]),
-                        //     babel.types.file(babel.types.program([[decorators[weaves[path.parent.id.name]]]])
-                        //         , "", "")
-                        // ));
-
-                        // console.log(decorators[weaves[path.parent.id.name]]);
-                        
-
-                        // path.parentPath.parentPath.insertAfter(babel.types.variableDeclaration("let", [
-                        //     babel.types.variableDeclarator(
-                        //         babel.types.tsParameterProperty(babel.types.identifier(weaves[path.parent.id.name]))
-                        //     )
-                        // ]));
-
-                        // path.insertAfter(decorators[weaves[path.parent.id.name]]);
-                    // }
                 }
             }
         }]});
 
         console.log(out2.code);
-      
-        // console.log(out.code);
     });
 });
